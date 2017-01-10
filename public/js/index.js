6fcb3dcd71be012d0cfce371820a0921e7277fb1
Alex <alexrichards92@me.com>
2017-01-10 11:17:41 -0800

    click to scroll feature added

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,9 @@
 window.onload = function () {
 
+  var portfolioImages = [];
+
   var socket = io();
   socket.connect("http://localhost:3001/socket.io.js");
-
-  // Swaps the main photo when the menu bar is fully exposed/hidden.
-  $(window).scroll(function() {
-    url = $('#main-container').css('background-image');
-    if ($(window).scrolltop() == 0 && url.indexof('headshot_light') !== -1) {
-      swapbackground('url(../images/new_photo.jpg) no-repeat -15em center fixed', function() {});
-    }
-  });
-
 
   // Click event handler for exposing subsections of the site.
   $('.link').click(function(e) {
@@ -27,6 +20,51 @@ window.onload = function () {
       swapBackground('url(../images/Bandelion_dark.jpg) no-repeat center center fixed', changeSection);
     } else if (this.id == 'contact') {
       swapBackground('url(../images/Bandelion_light.jpg) no-repeat center center fixed', changeSection);
+    }
+  });
+
+
+  // Click event to scroll to bottom of reels page.
+  $('#down-arrow').click(function(e) {
+    $('body').animate({scrollTop: document.body.scrollHeight}, 500);
+  });
+
+  // Click event handler for swapping reel clips.
+  reels = {
+    'summaries': ['Qi Aerista Kickstarter Promo', 'Avid Dance Company Promo'],
+    'clips': ['https://www.youtube.com/embed/nIS6qMorab4','https://www.youtube.com/embed/iYZZ6d-JrtU']
+  }
+  reelNum = 0;
+  $('#left-arrow').click(function(e) {
+    $('#scroll-player').attr('src', reels.clips[--reelNum % reels.clips.length]);
+    $('#reel-info').text(reels.summaries[reelNum % reels.summaries.length]);
+  });
+  $('#right-arrow').click(function(e) {
+    $('#scroll-player').attr('src', reels.clips[++reelNum % reels.clips.length]);
+    $('#reel-info').text(reels.summaries[reelNum % reels.summaries.length]);
+  });
+
+
+  // Click event for mini-images to scroll full-sized image-container
+  $(document).on('click', '.mini-image', function() {
+    var id = this.id.substring(5);
+    var position = $('#'+id).position().left;
+    var halfImage = $('#'+id).width()/2;
+    var windowWidth = window.innerWidth/2;
+    $('#images-container').animate({scrollLeft: $('#images-container').scrollLeft() + position - windowWidth + halfImage}, 400);
+  });
+
+
+  // Fills image-container with divs for each image.
+  socket.on('imageResponse', function (obj) {
+    portfolioImages = obj.files;
+    for (i=0; i < obj.files.length; i++) {
+      var img = "<img id='img-"+i+"' class='image' src='images/portfolio/"+obj.files[i]+"\'>";
+      $('#images-container').html($('#images-container').html() + "\n" + img);
+    }
+    for (j=0; j < obj.thumbs.length; j++) {
+      var thumb = "<img id='mini-img-"+j+"' class='image mini-image' src='images/portfolio/thumbnails/"+obj.thumbs[j]+"\'>";
+      $('#mini-images-container').html($('#mini-images-container').html() + "\n" + thumb);
     }
   });
 
@@ -63,46 +101,4 @@ window.onload = function () {
       }
     });
   }
-
-
-  // Fills image-container with divs for each image.
-  socket.on('imageResponse', function (obj) {
-    for (i=0; i < obj.files.length; i++) {
-      var img = "<img class='image-container' src='images/portfolio/"+obj.files[i]+"\'>";
-      $('#images-container').html($('#images-container').html() + "\n" + img);
-    }
-    for (j=0; j < obj.thumbs.length; j++) {
-      var thumb = "<img class='image-container' src='images/portfolio/thumbnails/"+obj.thumbs[j]+"\'>";
-      $('#mini-images-container').html($('#mini-images-container').html() + "\n" + thumb);
-    }
-    alert("wrapper1: " + $("#wrapper1").css("height") + "\n" + "images-container: " + $("#images-container").css("height") + "\n" + "wrapper2: " + $("#wrapper1").css("height") + "\n" + "mini-images-container: " + $("#mini-images-container").css("height") + "\n" + "image: " + $(".image-container").css("height"));
-  });
-
-  $("#wrapper2").scroll(function(e) {
-    alert('mini scrolling');
-  });
-
-
-  // Click event to scroll to bottom of reels page.
-  $('#down-arrow').click(function(e) {
-    $('body').animate({scrollTop: document.body.scrollHeight}, 500);
-  });
-
-
-  // Click event handler for swapping reel clips.
-  reels = {
-    'summaries': ['Qi Aerista Kickstarter Promo', 'Avid Dance Company Promo'],
-    'clips': ['https://www.youtube.com/embed/nIS6qMorab4','https://www.youtube.com/embed/iYZZ6d-JrtU']
-  }
-  reelNum = 0;
-  $('#left-arrow').click(function(e) {
-    $('#scroll-player').attr('src', reels.clips[--reelNum % reels.clips.length]);
-    $('#reel-info').text(reels.summaries[reelNum % reels.summaries.length]);
-  });
-  $('#right-arrow').click(function(e) {
-    $('#scroll-player').attr('src', reels.clips[++reelNum % reels.clips.length]);
-    $('#reel-info').text(reels.summaries[reelNum % reels.summaries.length]);
-  });
-
-  //$('#reels').trigger('click');
 }
